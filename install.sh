00a6b277612f5015dcdfbc1155d6252461365d9c
Glavin Wiechert <glavin.wiechert@gmail.com>
2013-11-21 14:56:59 -0400

    See #9. Improved Automated installation script.

--- install.sh
+++ install.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
+# === Dependencies
 # CocoaPods
 command -v pod >/dev/null 2>&1 || { 
     echo "Installing CocoaPods"
     sudo gem install cocoapods
     pod setup
+    echo
 }
-
 # Appledoc
 command -v appledoc >/dev/null 2>&1 || { 
     echo "Installing Appledoc"
     brew install appledoc
+    echo
 }
 
-echo "Installation Completd."
+# === Install
+echo "===== Cocoa/Objective-C SDK ====="
+echo "Installing CocoaPods"
+cd Streamlyne-iOS-SDK/ 
+pod install
+cd ../
+echo "Open in Xcode the Streamlyne-iOS-SDK/Streamlyne-iOS-SDK.xcworkspace"
+echo
 
+# === Update
+echo "Initializing and Updating Git Submodules"
+git submodule update --init --recursive
+
+echo "Installation Completed."
+echo
