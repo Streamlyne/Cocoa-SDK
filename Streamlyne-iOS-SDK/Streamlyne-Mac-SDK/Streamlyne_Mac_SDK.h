4fc2c3a64e2871a30425e5b6108240f05a9c7720
Glavin Wiechert <Glavin.Wiechert@gmail.com>
2013-11-23 04:24:19 -0400

    Closes #12. Added Mac SDK support, along to the existing iOS SDK.

---
+++ Streamlyne-iOS-SDK/Streamlyne-Mac-SDK/Streamlyne_Mac_SDK.h
@@ -0,0 +1,13 @@
+//
+//  Streamlyne_Mac_SDK.h
+//  Streamlyne-Mac-SDK
+//
+//  Created by Glavin Wiechert on 11/23/2013.
+//  Copyright (c) 2013 Streamlyne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Streamlyne_Mac_SDK : NSObject
+
+@end
---
+++ Streamlyne-iOS-SDK/Streamlyne-Mac-SDK/Streamlyne_Mac_SDK.m
@@ -0,0 +1,13 @@
+//
+//  Streamlyne_Mac_SDK.m
+//  Streamlyne-Mac-SDK
+//
+//  Created by Glavin Wiechert on 11/23/2013.
+//  Copyright (c) 2013 Streamlyne. All rights reserved.
+//
+
+#import "Streamlyne_Mac_SDK.h"
+
+@implementation Streamlyne_Mac_SDK
+
+@end
