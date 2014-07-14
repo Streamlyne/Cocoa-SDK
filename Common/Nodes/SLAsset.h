2b0c5b605bf432d2ae4b53e15da7e579ea92dc2f
Glavin Wiechert <glavin@streamlyne.co>
2014-07-14 15:29:52 -0300

    See #4. Worked on Assets Model.

--- Common/Nodes/SLAsset.h
+++ Common/Nodes/SLAsset.h
@@ -2,19 +2,31 @@
 //  SLAsset.h
 //  Streamlyne Cocoa SDK
 //
-//  Created by Glavin Wiechert on 11/24/2013.
-//  Copyright (c) 2013 Streamlyne. All rights reserved.
+//  Created by Glavin Wiechert on 2014-07-14.
+//  Copyright (c) 2014 Streamlyne. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
 #import "SLNode.h"
+
 
 @interface SLAsset : SLNode
 
-@property (nonatomic, retain) NSString * assetNumber;
+@property (nonatomic, retain) NSString * name;
 @property (nonatomic, retain) NSString * costCenter;
 @property (nonatomic, retain) NSString * desc;
 @property (nonatomic, retain) NSString * location;
 @property (nonatomic, retain) NSString * mfg;
 @property (nonatomic, retain) NSString * serial;
+@property (nonatomic, retain) NSSet *attributes;
+@end
+
+@interface SLAsset (CoreDataGeneratedAccessors)
+
+- (void)addAttributesObject:(NSManagedObject *)value;
+- (void)removeAttributesObject:(NSManagedObject *)value;
+- (void)addAttributes:(NSSet *)values;
+- (void)removeAttributes:(NSSet *)values;
 
 @end
--- Common/Nodes/SLAsset.m
+++ Common/Nodes/SLAsset.m
@@ -2,37 +2,21 @@
 //  SLAsset.m
 //  Streamlyne Cocoa SDK
 //
-//  Created by Glavin Wiechert on 11/24/2013.
-//  Copyright (c) 2013 Streamlyne. All rights reserved.
+//  Created by Glavin Wiechert on 2014-07-14.
+//  Copyright (c) 2014 Streamlyne. All rights reserved.
 //
 
 #import "SLAsset.h"
 
+
 @implementation SLAsset
 
-@dynamic assetNumber;
+@dynamic name;
 @dynamic costCenter;
 @dynamic desc;
 @dynamic location;
 @dynamic mfg;
 @dynamic serial;
-
-+(NSString *) type
-{
-    return @"asset";
-}
-
-
-+ (NSDictionary *) attributeMappings
-{
-    NSMutableDictionary *attrMap = [NSMutableDictionary dictionaryWithDictionary:[[[self superclass] class] attributeMappings]];
-    [attrMap setValue:@"assetNumber" forKey:@"number_asset"];
-    [attrMap setValue:@"serial" forKey:@"number_serial"];
-    [attrMap setValue:@"desc" forKey:@"description"];
-    [attrMap setValue:@"mfg" forKey:@"mfg"];
-    [attrMap setValue:@"location" forKey:@"location"];
-    [attrMap setValue:@"costCenter" forKey:@"cost_center"];
-    return [NSDictionary dictionaryWithDictionary: attrMap];
-}
+@dynamic attributes;
 
 @end
